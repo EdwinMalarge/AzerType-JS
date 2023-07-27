f3fda44bd29e7c9b1718fe99db8d7b855920556b
EdwinMalarge <edwin.malarge@outlook.com>
2023-07-27 15:58:42 +0200

    //

---
+++ script.js
@@ -0,0 +1,13 @@
+const motApplication = "Bonjour"
+
+
+let motUtilisateur = prompt("entrez le mot indiqué")
+    console.log (motUtilisateur)
+
+
+if (motUtilisateur === motApplication) {
+    console.log ("Bravo, bien joué")}
+    else {
+        console.log("Il y a une erreur")
+    }
+    
