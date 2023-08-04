606c4ffe9ee0672860adddb31497e047f98f0bc6
EdwinMalarge <edwin.malarge@outlook.com>
2023-08-04 13:59:03 +0200

    //

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,29 @@
-lancerjeu ()
+lancerJeu ()
 
 
-let zoneSaisie = document.getElementById("zoneSaisie")
-console.log ("zoneSaisie")
+let saisie = document.getElementById("zoneSaisie")
+
+
+let proposition = document.querySelector(".zoneProposition")
+let score = document.querySelector(".zoneScore")
+
+let boutonRadio = document.querySelectorAll(".optionSource input")
+
+console.log (saisie)
+
+
+//corrigé
+let inputEcriture = document.getElementById("inputEcriture")
+console.log(inputEcriture)
+
+let btnValiderMot = document.getElementById("btnValiderMot")
+console.log(btnValiderMot)
+
+let zoneProposition = document.querySelector(".zoneProposition")
+console.log(zoneProposition)
+
+let spanScore = document.querySelector(".zoneScore span")
+console.log(spanScore)
+
+let listeBtnRadio = document.querySelectorAll(".optionSource input")
+console.log(listeBtnRadio)
