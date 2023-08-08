d809f7fc77bf5f098255ed22161f2d8ff115f6c7
EdwinMalarge <edwin.malarge@outlook.com>
2023-08-08 10:53:17 +0200

    //

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,5 @@
 lancerJeu()
 
-//let inputEcriture = document.getElementById("inputEcriture")
-//console.log(inputEcriture)
-
-//let btnValiderMot = document.getElementById("btnValiderMot")
-//console.log(btnValiderMot)
-
-//let zoneProposition = document.querySelector(".zoneProposition")
-//console.log(zoneProposition)
-
 //let spanScore = document.querySelector(".zoneScore span")
 //console.log(spanScore)
 
